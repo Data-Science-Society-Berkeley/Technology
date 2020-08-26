3be1d4756e21bd1e699d19bc52f8ebd9c648282a
Arjun Mishra <arjunmishra@arjuns-mbp-312.lan>
2020-08-26 13:08:57 -0700

    updated footer + compressed

--- client/src/footer.js
+++ client/src/footer.js
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Card, Header,Button, Form, Input, Icon,List,Segment,Container,Grid } from "semantic-ui-react";
+import { Card, Header,Button, Form, Input, Icon,List,Segment,Container,Grid,Image } from "semantic-ui-react";
 import { withRouter, BrowserRouter as Router, Switch, Route, Link  } from 'react-router-dom';
+import logo from './Backgrounds/DSS-logo-black-transparent.png'
+import ocf from  './Backgrounds/ocf-hosted-penguin.svg'
 
 let endpoint = "http://localhost:8080/test";
 const footer = {
-  color: "#FFFFFF",
-  fontFamily: "Rubik",
-  fontSize: "16px",
-  letterSpacing: "0.57px",
-  lineHeight: "30px",
+  color: "#000000",
+  fontFamily: "Montserrat",
+  fontSize: "14px",
+  fontStyle:"normal",
+  lineHeight: "17px",
+};
+const iconDesc = {
+  color: "#000000",
+  fontFamily: "Montserrat",
+  fontSize: "18px",
+  fontStyle:"normal",
+  lineHeight: "22px",
+  marginLeft:"18px",
+  marginTop:"0px",
+  verticalAlign:"middle"
+};
+const link = {
+  color: "#000000",
+  fontFamily: "Montserrat",
+  fontSize: "14px",
+  fontStyle:"normal",
+  lineHeight: "17px",
 };
 class Footer extends Component {
   constructor(props) {
@@ -17,75 +36,116 @@ class Footer extends Component {
   }
   render() {
     return (
-      <Segment vertical style={{ width: '100%',marginTop: '0px',marginBottom: '0px',height: '450px',background: '#FFFFFF' }}>
+      <Segment vertical style={{ width: '100%',marginTop: '0px',marginBottom: '0px',height: '600px',background: '#E5E5E5' }}>
       <Container>
         <Grid divided inverted stackable>
-          <Grid.Row>
-          <Grid.Column width={2}>
-          <Icon.Group size='huge' style={{marginLeft:"0px",marginTop:"0px"}}>
-                        <a href="mailto:info@dss.berkeley.edu"><Icon name="mail" style={{marginLeft:"100px",color:"#8CD6D1"}}>
-
+        <Grid.Row>
+        <div style={{marginLeft:"-493px",marginTop:"24px"}}>
+          <Image inline src={logo} style={{width:"74px",height:"74px"}}>
+          </Image>
+          <div style={{marginLeft:"100px",marginTop:"-40px",fontFamily:"Montserrat",fontStyle:"normal",fontWeight:"300px",fontSize:"24px",lineHeight:"29px"}}>
+            DATA SCIENCE SOCIETY
+          </div>
+          </div>
+        </Grid.Row>
+          <Grid.Row style={{marginBottom:"10px",marginTop:"24px"}}>
+          <Grid.Column width={8}>
+          <Icon.Group size='huge' style={{marginLeft:"-450px",marginTop:"0px"}}>
+                        <a href="mailto:info@dss.berkeley.edu"><Icon name="mail outline" style={{marginBottom:"14px",color:"#8CD6D1"}}>
                           </Icon>
                           </a>
-                          <a href="https://www.instagram.com/dssberkeley/"> <Icon name="instagram" style={{marginLeft:"0px",color:"#8CD6D1"}}/> </a>
+                          <span style={iconDesc}><span style={{marginLeft:"-8px"}}> Email</span> </span>
+                          <br/>
+                          <a href="https://www.instagram.com/dssberkeley/"> <Icon name="instagram" style={{marginBottom:"14px",color:"#8CD6D1"}}/>
+                          <span style={iconDesc}> Instagram</span>
+
+                           </a>   <br/>
 
                          
-                          <a href="https://www.facebook.com/dssberkeley"> <Icon name="facebook" style={{marginLeft:"10px",color:"#8CD6D1"}}>
+                          <a href="https://www.facebook.com/dssberkeley"> <Icon name="facebook" style={{marginBottom:"14px",color:"#8CD6D1"}}>
 
                             </Icon>
+                            <span style={iconDesc}> Facebook</span>
+
                             </a>
-                            <a href="https://www.linkedin.com/company/data-science-society-at-berkeley/"> <Icon name="linkedin" style={{marginLeft:"10px",color:"#8CD6D1"}}>
+                            <br/>
+                            <a href="https://www.linkedin.com/company/data-science-society-at-berkeley/"> <Icon name="linkedin" style={{marginBottom:"14px",color:"#8CD6D1"}}>
 
                             </Icon>
+                            <span style={iconDesc}> LinkedIn</span>
                             </a>
-                            <a href="https://www.youtube.com/channel/UCBj7xVI8e_jlEAznyVetWIQ/"> <Icon name="youtube" style={{marginLeft:"10px",color:"#8CD6D1"}}>
+                            <br/>
+                            <a href="https://www.youtube.com/channel/UCBj7xVI8e_jlEAznyVetWIQ/"> <Icon name="youtube" style={{marginBottom:"10px",color:"#8CD6D1",marginLeft:"-8px"}}>
 
                             </Icon>
+                            <span style={iconDesc}> <span style={{marginLeft:"-8px"}}>YouTube</span></span>
                             </a>
+                            <br/>
                         </Icon.Group>
           </Grid.Column>
-            <Grid.Column width={3}>
-              <div  class='lumber-io' >
-              General
+            <Grid.Column width={2} >
+              <div  style={{link}}>
+              <span style={{fontWeight:"450"}}>General </span>
                   </div>
               <List link inverted>
-                <List.Item as='a' style={footer}> Past Work </List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-            <div  class='info' >
-                INFO
-            </div>
-              <List link inverted>
-                <List.Item as='a' style={footer}> Terms of Use</List.Item>
-                <List.Item as='a' style={footer}>Privacy Policy</List.Item>
-                <List.Item as='a' style={footer}>Knowledge Base</List.Item>
-                <List.Item as='a' style={footer}>Help Desk</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-            <div  class='info' >
-                SUPPORT
-            </div>
-            <List link inverted>
-              <List.Item as='a' style={footer}> Help Desk </List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={6}>
-                <Icon inverted size={"big"} style={{ width: '31px',height: '37px' }} name='facebook'/> 
-                <Icon  inverted size={"big"} style={{ width: '31px',height: '37px' }} name='instagram'/> 
-                <Icon  inverted size={"big"} style={{ width: '31px',height: '37px' }} name='twitter'/> 
-                <Icon inverted size={"big"} style={{ width: '31px',height: '37px' }} name='youtube'/> 
+                <Link to="/about"> <List.Item as='a' style={footer}> About us </List.Item></Link>
+                <br/>                 <br/>
 
+                <Link to="/acadev"><List.Item as='a' style={footer}> AcaDev</List.Item></Link>
+                <br/>                
+
+
+                <Link to="/consulting"><List.Item as='a' style={footer}> Consulting </List.Item></Link>
+                <br/>              
+                <Link to='/decal'> <List.Item as='a' style={footer}> DeCal </List.Item></Link>
+                <br/>
+                <Link to="socialgood"> <List.Item as='a' style={footer}> Social Good </List.Item></Link>
+                <br/>                <br/>
+
+
+                <Link to="/services"> <List.Item as='a' style={footer}> Services </List.Item></Link>
+
+
+
+              </List>
             </Grid.Column>
+            <Grid.Column width={2}>
+            <div   style={{link}}>
+            <span style={{fontWeight:"450"}}>Past Work     </span>       </div>
+              <List link inverted>
+                <Link to="/consulting">
+              <List.Item as='a' style={footer}> Consulting</List.Item>  </Link>
+                <Link to="/events"><List.Item as='a' style={footer}>Events</List.Item> </Link>
+                <Link to="/events"> <List.Item as='a' style={footer}>Workshops</List.Item></Link>
+                <List.Item as='a' style={footer}>GM Projects</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={2}>
+            <div  style={{link}} >
+            <span style={{fontWeight:"450"}}> Program Pages  </span>          </div>
+              <List link inverted>
+                <Link to="/gm"><List.Item as='a' style={footer}> General Membership </List.Item></Link>
+                <br/>                <br/>
+
+                <Link to='/decallearn'><List.Item as='a' style={footer}>DeCal: Essential Tools for Data Science </List.Item></Link>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={2}>
+            <div   style={{link}}>
+            <span style={{fontWeight:"450"}}> Contact Us       </span>   </div>
+              <List link inverted>
+                <Link to="/apply"><List.Item as='a' style={footer}> <span style={{fontWeight:"480",color:"#000000"}}>Apply</span></List.Item></Link>
+              </List>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row centered style={{marginBottom:"22px"}}>
+          <div style={footer}>
+          We are a student group acting independently of the University of California. We take full responsibility for our organization and this website.
+
+          </div>
           </Grid.Row>
         </Grid>
       </Container>
-      <div style={{ textAlign: 'center',color: "#BBBBBB", fontFamily: "Rubik",
-      fontSize: "16px",letterSpacing: "0.57px",lineHeight: "19px",marginTop: '30px'}}>
-        <Icon name='copyright outline'/>
-      Copyright 2019. All rights reserved
-      </div>
     </Segment>
     )
 
