d2dde1b6c687e296daa20293347b264e7dec8b47
Labanya Mukhopadhyay <labanyam@hotmail.com>
2021-11-08 17:49:40 -0800

    update zoom link for datathon

--- client/src/App.js
+++ client/src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
           <Route
             path="/zoom"
             component={() => {
-              window.location.href = "https://berkeley.zoom.us/j/93293612130";
+              window.location.href = "https://berkeley.zoom.us/j/91729580693";
               return null;
             }}
           />
@@ -453,7 +453,7 @@ class App extends Component {
           <Route
             path="/zoom"
             component={() => {
-              window.location.href = "https://berkeley.zoom.us/j/93293612130";
+              window.location.href = "https://berkeley.zoom.us/j/91729580693";
               return null;
             }}
           />
