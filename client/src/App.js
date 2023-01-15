9d432983551c24aace724fa422e39e265e93e6fa
Kuhu Sharma <kuhusharma@berkeley.edu>
2023-01-14 16:06:36 -0800

    a whole bunch of interesting half done works

--- client/src/App.js
+++ client/src/App.js
@@ -61,6 +61,7 @@ class App extends Component {
       projectname: "Project Name",
       clientid: "Client Name",
       projectid: "Client Name",
+      currentpage: ""
     };
   }
   sendData(data) {
@@ -182,12 +183,12 @@ class App extends Component {
             path="/"
             render={(props) => (
               <div>
-                <NavBar {...props} />
                 <CentralText
                   style={{ paddingBottom: "500px" }}
                   {...props}
                   buttonClick={this.sendData.bind(this)}
                 />
+                <NavBar {...props} />
                 <Footer></Footer>
               </div>
             )}
@@ -532,12 +533,12 @@ class App extends Component {
             path="/"
             render={(props) => (
               <div>
-                <NavBar {...props} />
                 <CentralText
                   style={{ paddingBottom: "500px" }}
                   {...props}
                   buttonClick={this.sendData.bind(this)}
                 />
+                <NavBar {...props} />
                 <Footer></Footer>
               </div>
             )}
