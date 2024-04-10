6380aa92fa94c7a3958d599685f65c81c927f662
Harshit Jain <harshitjain153@berkeley.edu>
2024-04-10 15:46:49 -0700

    updated landing page banner

--- client/src/CentralText.js
+++ client/src/CentralText.js
@@ -26,7 +26,8 @@ import rectangleGroup from "./landing/rectangleGroup.png";
 import squiggly1 from "./landing/squiggly1.png";
 import squiggly2 from "./landing/squiggly2.png";
 /* import homepageBanner from './Homepage/homepage.png'; */
-import homepageBanner from './Homepage/landing_photo_sp24_3.jpg';
+// import homepageBanner from './Homepage/landing_photo_sp24_3.jpg';
+import homepageBanner from './Homepage/landing_photo_sp24_title.png';
 import rightarrow from './Homepage/ArrowRight.png'
 import stats from './Homepage/stats.png'
 import gradientOne from './Homepage/homepagegradient1.png';
