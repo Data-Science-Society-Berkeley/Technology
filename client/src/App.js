cbed6f0fa92428afaaf38eaabaf1085b87729bf3
Austin <ardisaputra.2003@gmail.com>
2024-07-31 08:34:34 -0700

    Added routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,69 +1,29 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Navbar from "./components/Navbar.js";
 import Footer from "./components/Footer.js";
-
-const headlineStats = [
-  { name: "past decal students", num: "1000+" },
-  { name: "past decal students", num: "1000+" },
-  { name: "past decal students", num: "1000+" },
-  { name: "past decal students", num: "1000+" },
-  { name: "past decal students", num: "1000+" },
-  { name: "past decal students", num: "1000+" },
-];
+import About from "./subpages/About.js";
+import Home from "./subpages/Home.js";
+import Decal from "./subpages/Decal.js";
+import Events from "./subpages/Events.js";
+import Services from "./subpages/Services.js";
+import JoinUs from "./subpages/JoinUs.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App bg-gradient-to-b from-white to-[#8CD6D1]">
+    <BrowserRouter>
       <Navbar />
-      <img src={"/landingphoto.png"} alt="Landing Photo" />
-      <div className="flex flex-row flex-wrap items-center justify-center">
-        <div className="p-4 m-2 text-black text-left font-lato flex-1 min-w-[20em]">
-          <div className="text-4xl font-bold">DATA SCIENCE SOCIETY</div>
-          <br />
-          <div className="text-m">
-            Berkeley’s first undergraduate data-science focused student
-            organization. Actively fostering a strong data science community on
-            campus via our consulting and educational initiatives.
-          </div>
-          <br />
-          About us →
-        </div>
-        <div className="bg-green-500 m-2 text-white flex-1 min-w-[20em]">
-          <div className="grid grid-cols-3 gap-4 p-4">
-            {headlineStats.map((item) => (
-              <div className="flex flex-col">
-                <div className="bg-gray-200 p-1">TEST</div>
-                <div className="bg-gray-200">{item.name}</div>
-              </div>
-            ))}
-          </div>
-        </div>
+      <div className="App bg-gradient-to-b from-white to-[#8CD6D1]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/decal" element={<Decal />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/joinus" element={<JoinUs />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
       </div>
-      <img src={"/pattern1.png"} alt="Landing Photo" />
-      <div className="flex flex-row items-center justify-center">
-        <div className="bg-blue-500 p-4 m-2 text-white font-lato">Item 1</div>
-        <div className="bg-green-500 p-4 m-2 text-white">Item 2</div>
-      </div>
-      <div className="flex flex-row items-center justify-center">
-        <div className="bg-blue-500 p-4 m-2 text-white font-lato">Item 1</div>
-        <div className="bg-green-500 p-4 m-2 text-white">Item 2</div>
-      </div>
-      <div className="flex flex-row items-center justify-center">
-        <div className="bg-blue-500 p-4 m-2 text-white font-lato">Item 1</div>
-        <div className="bg-green-500 p-4 m-2 text-white">Item 2</div>
-      </div>
-      <div className="flex flex-row items-center justify-center">
-        <div className="bg-blue-500 p-4 m-2 text-white font-lato">Item 1</div>
-        <div className="bg-green-500 p-4 m-2 text-white">Item 2</div>
-      </div>
-      <div className="flex flex-row items-center justify-center">
-        <div className="bg-blue-500 p-4 m-2 text-white font-lato">Item 1</div>
-        <div className="bg-green-500 p-4 m-2 text-white">Item 2</div>
-      </div>
-      <img src={"/pattern2.png"} alt="Landing Photo" />
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/subpages/JoinUs.js
@@ -0,0 +1,9 @@
+function JoinUs() {
+  return (
+    <div className="bg-gradient-to-b from-white to-[#8CD6D1]">
+      <h1>testststs</h1>
+    </div>
+  );
+}
+
+export default JoinUs;
