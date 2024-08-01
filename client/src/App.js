adb1230ba0a38657f21db7909a34bb51b9416432
Austin <ardisaputra.2003@gmail.com>
2024-08-01 00:52:46 -0700

    Add Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
