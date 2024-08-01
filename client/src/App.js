71de6ae8edd5db7d03f9ce5b6b1917f0ff62bc1c
Austin <ardisaputra.2003@gmail.com>
2024-08-01 00:29:11 -0700

    Bruh i can't get access to netlify wht to do

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,24 @@
-import Footer from "./components/Footer.js";
-import About from "./subpages/About.js";
-import Home from "./subpages/Home.js";
-import Decal from "./subpages/Decal.js";
-import Events from "./subpages/Events.js";
-import Services from "./subpages/Services.js";
-import JoinUs from "./subpages/JoinUs.js";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App bg-gradient-to-b from-white to-[#8CD6D1]">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/decal" element={<Decal />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/joinus" element={<JoinUs />} />
-          <Route path="/services" element={<Services />} />
-        </Routes>
-      </div>
-      <Footer />
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
