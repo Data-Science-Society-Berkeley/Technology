e25385ced581a93ed352de07d4ea07c8edc27dad
Austin Nicola Ardisaputra <ardisaputra.2003@gmail.com>
2025-01-17 08:50:30 +0700

    Change home file

--- client/src/subpages/Home.js
+++ client/src/subpages/Home.js
@@ -53,66 +53,7 @@ const landingPageIndex = [
 ];
 
 function Home() {
-  return (
-    <div>
-      <img
-        class="h-full w-full object-cover"
-        src={"/landingphoto.png"}
-        alt="Landing"
-      />
-      <div className="flex flex-row flex-wrap items-center justify-center">
-        <div className="p-4 mx-[4em] mt-[2em] text-black text-left font-lato flex-1 min-w-[20em]">
-          <div className="text-4xl font-bold">DATA SCIENCE SOCIETY</div>
-          <br />
-          <div className="text-m">
-            Berkeley’s first undergraduate data-science focused student
-            organization. Actively fostering a strong data science community on
-            campus via our consulting and educational initiatives.
-          </div>
-          <br />
-          <Link to="/about" className="text-[#77b8b3]">
-            About us →
-          </Link>
-        </div>
-        <div className="mx-[4em] mt-[2em] text-black flex-1 min-w-[20em]">
-          <div className="grid grid-cols-3 gap-4 p-4">
-            {headlineStats.map((item) => (
-              <div className="flex flex-col items-center">
-                <div className="p-1 sm:text-5xl text-3xl text-[#77b8b3] font-lato font-bold">
-                  {item.num}
-                </div>
-                <div className="text-center">{item.name}</div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-      <img src={"/pattern1.png"} alt="Pattern 1" />
-      {landingPageIndex.map((item) => (
-        <div className="flex flex-row items-center justify-center flex-wrap">
-          <div className="p-4 m-2 text-black text-left font-lato flex-grow max-w-[30em]">
-            <div className="text-4xl font-bold">{item.keyword}</div>
-            <br />
-            <div className="text-xl font-bold">{item.subtitle}</div>
-            <br />
-            <div className="text-m">{item.desc}</div>
-            <br />
-            <Link to={item.url} className="text-blue-500">
-              {item.link}
-            </Link>
-          </div>
-          <div className="p-4 m-2 text-white">
-            <img
-              className="md:w-[30em] w-[15em]"
-              src={item.img}
-              alt={item.img}
-            />
-          </div>
-        </div>
-      ))}
-      <img src={"/pattern2.png"} alt="Pattern 2" />
-    </div>
-  );
+  return <div>We have moved</div>;
 }
 
 export default Home;
